be0feed5d74b0f40f0709c34764f4e95b798385b
vladov <vladov3000@gmail.com>
2020-09-14 13:08:17 -0700

    Update MajorityVote.java

--- MajorityVote.java
+++ MajorityVote.java
@@ -5,6 +5,22 @@ class Solution {
     public int solve(int[] nums) {
         Arrays.sort(nums);
         
+        int combo = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                combo++;
+            } else {
+                if (combo > nums.length / 2) return nums[i-1];
+                combo = 1;
+            }
+        }
+        if (combo > nums.length / 2) return nums[nums.length-1];
+        return -1;
+    }
+    
+    public int solve1(int[] nums) {
+        Arrays.sort(nums);
+        
         int start = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i-1] != nums[i]) {
