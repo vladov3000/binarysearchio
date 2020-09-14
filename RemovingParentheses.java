a72f1b90f48982e9a3bca2673b19587248d309b6
vladov <vladov3000@gmail.com>
2020-09-14 13:25:49 -0700

    Update RemovingParentheses.java

--- RemovingParentheses.java
+++ RemovingParentheses.java
@@ -3,12 +3,26 @@ import java.util.*;
 
 class Solution {
     public int solve(String s) {
+        int res = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else {
+                if (count == 0) res++;
+                else count--;
+            }
+        }
+        return res + count;
+    }
+    
+    public int solve1(String s) {
         Stack<Character> p = new Stack<>();
         int res = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 p.push(s.charAt(i));
-            } else if (s.charAt(i) == ')') {
+            } else {
                 if (p.empty()) res++;
                 else p.pop();
             }
